5d2e064a3ce2ace796bfa25ef10b358374585e21
admin <chiinhuv@gmail.com>
2018-07-24 14:47:11 +0700

    add

--- passingDataByDelegeteUseExtention/ViewController.swift
+++ passingDataByDelegeteUseExtention/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: [Int] = Array(1...5)
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+               return number.count
+    }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = String(number[indexPath.row])
+            return cell
+        }
+    
+}
+extension ViewController: UITableViewDelegate {
+    
+}
+
 
